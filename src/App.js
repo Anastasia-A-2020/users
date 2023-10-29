33b546c38390851e85f5a3f0fba3d2afec057fe8
Anastasia Aleksandrova <anastasiayurievnaaleksandrova@gmail.com>
2023-10-29 21:23:29 +0200

    implemented functionality for inviting users

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [serchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then(res => res.json())
+      .then(json => setUsers(json.data))
+      .catch(err => {
+        console.log(err);
+        alert("Error fetching data");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = e => {
+    setSearchValue(e.target.value);
+  };
+
+  const onClickInvite = id => {
+    console.log(id);
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id !== id));
+    } else {
+      setInvites(prev => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  };
+
+  return (
+    <div className="App">
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          items={users}
+          isLoading={isLoading}
+          searchValue={serchValue}
+          onChangeSearchValue={onChangeSearchValue}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Success.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Success = ({ count }) => {
+  return (
+    <div className="success-block">
+      <img src="/assets/success.svg" alt="Success" />
+      <h3>Успешно!</h3>
+      <p>Всем {count} пользователям отправлено приглашение.</p>
+      <button
+        onClick={() => window.location.reload()}
+        className="send-invite-btn"
+      >
+        Назад
+      </button>
+    </div>
+  );
+};
+
+Success.propTypes = {
+  count: PropTypes.number.isRequired,
+};
